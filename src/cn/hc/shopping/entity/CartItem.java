09dac4f443079e8e592def6545449bc059d11d67
liuchaojie-work <liuchaojie_work@163.com>
2020-02-07 13:56:37 +0800

    实体类CartItem

---
+++ src/cn/hc/shopping/entity/CartItem.java
@@ -0,0 +1,123 @@
+package cn.hc.shopping.entity;
+/**
+ * 购物车条目类
+ * @author L1471
+ *
+ */
+public class CartItem implements Comparable<CartItem>{
+	private int id;
+	private String name;
+	private double price;
+	private String color;
+	private int amount;
+	
+	public CartItem() {
+		super();
+	}
+
+	public CartItem(int id, String name, double price, String color, int amount) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.color = color;
+		this.amount = amount;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + amount;
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(price);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CartItem other = (CartItem) obj;
+		if (amount != other.amount)
+			return false;
+		if (color == null) {
+			if (other.color != null)
+				return false;
+		} else if (!color.equals(other.color))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "shoppingCarItem [id=" + id + ", name=" + name + ", price=" + price + ", color=" + color + ", amount="
+				+ amount + "]";
+	}
+
+	@Override
+	public int compareTo(CartItem o) {
+		// TODO Auto-generated method stub
+		return this.id-o.id;
+	}
+	
+	
+	
+}
